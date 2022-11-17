440df0a3cbaa4c2a7161a23d681e36cfd1caf5e2
anmolassi <assi.anmol@gmail.com>
2022-11-17 22:33:35 +0530

    SECOND COMMIT

--- routes/signin.js
+++ routes/signin.js
@@ -4,7 +4,7 @@ const search = require("../models/search");
 const router = express.Router();
 const app = express();
 const homeController = require("../controllers/home_controller");
-
+var userid;
 router.post("/", function (req, res) {
   console.log(req.body);
   User.findOne({ email: req.body.email }, function (err, user) {
@@ -16,6 +16,7 @@ router.post("/", function (req, res) {
       if (user.password != req.body.password) {
         return res.render("wrongCredentials");
       }
+      userid=user.id;
       res.cookie("user_id", user.id);
       search.find({}, function (err, searches) {
         if (err) {
@@ -33,16 +34,29 @@ router.post("/", function (req, res) {
 
 
 
-var list={search:'anmol'};
+
 router.get("/", function (req, res) {
   // return res.redirect("back");
-  return res.render("search", { search_list: list, user: User.findOne({_id:req.cookies.user_id}, function (err, user) {
+  res.cookie("user_id", userid);
+  var list;
+  search.find({},function (err, searches){
+    list=searches;
+    console.log(list);
+  });
+  var userr;
+  User.findOne({id:req.cookies.user_id}, function (err, user) {
     if (err) {
       console.log("Error in fetching USER from db");
       return;
     }
-  }) });
+    console.log(user);
+    userr=user;
+    console.log(userr);
+    return res.render("search", { search_list: list, user:  userr});
+  }); 
 });
+
+
 router.use("/filter", require("./search"));
 
 
@@ -57,11 +71,17 @@ router.get("/deleteSearch", function (req, res) {
     search.find({}, function (err, searches) {
       if (err) {
         console.log("Error in fetching contacts from db");
-        return;
+        return ;
       }
       console.log(searches);
-      return res.redirect("back");
-      // return res.render("search", { search_list: searches, user: user });
+      //return res.send(req.params);
+      var user=User.findOne({_id:req.cookies.user_id}, function (err, user) {
+            if (err) {
+              console.log("Error in fetching USER from db");
+              return;
+            }
+          });
+        res.render("search", { search_list: searches, user: user });
     });
     return res.redirect("back");
   });
