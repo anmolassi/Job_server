f0413753d79d9360527830b8318bc847f8581df4
anmolassi <assi.anmol@gmail.com>
2022-11-18 21:47:48 +0530

    TENTH COMMIT

--- routes/signin.js
+++ routes/signin.js
@@ -3,11 +3,11 @@ const User = require("../models/signup");
 const search = require("../models/search");
 const job = require("../models/jobs_post");
 const router = express.Router();
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const homeController = require("../controllers/home_controller");
 const userController = require("../controllers/user_controller");
-const passport =require('passport');
+const passport = require("passport");
 app.use(passport.initialize());
 app.use(passport.session());
 var userid;
@@ -23,58 +23,82 @@ router.post("/", function (req, res) {
       if (user.password != req.body.password) {
         return res.render("wrongCredentials");
       }
-      userid=user.id;
+      userid = user.id;
       res.cookie("user_id", user.id);
-      job.find({user_id:user._id}, function (err, searches) {
+      job.find({}, function (err, searches) {
         if (err) {
           console.log("Error in fetching contacts from db");
           return;
         }
-        joblist=searches;
+        joblist = searches;
       });
-      search.find({user_id:user._id}, function (err, searches) {
+      search.find({ user_id: user._id }, function (err, searches) {
         if (err) {
           console.log("Error in fetching contacts from db");
           return;
         }
         // return res.redirect("back");
-        return res.render("search", { search_list: searches, user: user,job_list:joblist });
+        var arr = [];
+        for (let i = 0; i < joblist.length; i++) {
+          for (let j = 0; j < searches.length; j++) {
+            if (searches[j].search == joblist[i].business_name) {
+              arr.push(joblist[i]);
+            }
+          }
+        }
+        return res.render("search", {
+          search_list: searches,
+          user: user,
+          job_list: arr,
+        });
       });
-      
     } else {
       return res.redirect("back");
     }
   });
 });
 
-router.get('/sign-out',userController.signOut);
-
+router.get("/sign-out", userController.signOut);
 
 router.get("/", function (req, res) {
   // return res.redirect("back");
   res.cookie("user_id", userid);
-  var list;
-  search.find({user_id:userid},function (err, searches){
-    list=searches;
-    //console.log(list);
-  });
-  var userr;
-  var cookieid=req.cookies.user_id;
-  console.log(cookieid);
-  User.findOne({_id:ObjectId(cookieid)}, function (err, user) {
+  job.find({}, function (err, searches) {
     if (err) {
-      console.log("Error in fetching USER from db");
+      console.log("Error in fetching contacts from db");
       return;
     }
-    userr=user;
-    return res.render("search", { search_list: list, user: user,job_list:joblist});
-  }); 
+    joblist = searches;
+  });
+  var list;
+  search.find({ user_id: userid }, function (err, searches) {
+    var arr = [];
+    list = searches;
+    for (let i = 0; i < joblist.length; i++) {
+      for (let j = 0; j < searches.length; j++) {
+        if (searches[j].search == joblist[i].business_name) {
+          arr.push(joblist[i]);
+        }
+      }
+    }
+    var userr;
+    var cookieid = req.cookies.user_id;
+    User.findOne({ _id: ObjectId(cookieid) }, function (err, user) {
+      if (err) {
+        console.log("Error in fetching USER from db");
+        return;
+      }
+      userr = user;
+      return res.render("search", {
+        search_list: list,
+        user: user,
+        job_list: arr,
+      });
+    });
+  });
 });
 
-
 router.use("/filter", require("./search"));
-
-
 
 router.get("/deleteSearch", function (req, res) {
   console.log(req.query);
@@ -82,24 +106,21 @@ router.get("/deleteSearch", function (req, res) {
     if (err) {
       console.log("ERORRR", err);
     }
-    console.log("aa chleya");
     search.find({}, function (err, searches) {
       if (err) {
         console.log("Error in fetching contacts from db");
-        return ;
+        return;
       }
-      console.log(searches);
       //return res.send(req.params);
-      var user=User.findOne({_id:req.cookies.user_id}, function (err, user) {
-            if (err) {
-              console.log("Error in fetching USER from db");
-              return;
-            }
-          });
-        // res.render("search", { search_list: searches, user: user,job_list:joblist });
+      User.findOne({ _id: req.cookies.user_id }, function (err, user) {
+        if (err) {
+          console.log("Error in fetching USER from db");
+          return;
+        }
+      });
+      return res.redirect("back");
     });
-    return res.redirect("back");
   });
 });
-router.use('/apply-job',require("./apply-job"))
+router.use("/apply-job", require("./apply-job"));
 module.exports = router;
